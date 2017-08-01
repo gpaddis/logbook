389f977f4634d91e6e50bd5b2b10739c89d38d43
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-08-01 16:32:59 +0200

    Add LiveCounter validation tests

--- app/Http/Controllers/LiveCounterController.php
+++ app/Http/Controllers/LiveCounterController.php
@@ -10,6 +10,14 @@ use Illuminate\Validation\Rule;
 class LiveCounterController extends Controller
 {
     /**
+     * ThreadsController constructor
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
--- tests/Feature/LiveCounterTest.php
+++ tests/Feature/LiveCounterTest.php
@@ -29,4 +29,52 @@ class LiveCounterTest extends TestCase
         $this->assertEquals($timeslot->start(), $entry->start_time);
         $this->assertEquals($timeslot->end(), $entry->end_time);
     }
+
+    /** @test */
+    public function an_unauthenticated_user_may_not_submit_a_visit_count()
+    {
+        $this->withExceptionHandling();
+
+        $this->get('/logbook/store?id=1&operation=add')
+            ->assertRedirect('login');
+    }
+
+    /** @test */
+    public function it_may_not_submit_an_invalid_operation()
+    {
+        $this->signIn()->withExceptionHandling();
+
+        create('App\PatronCategory', ['id' => 1]);
+
+        $this->get('/logbook/store?id=1&operation=deleteallrecords')
+            ->assertSessionHasErrors('operation');
+    }
+
+    /** @test */
+    public function it_may_not_submit_an_invalid_patron_category_id()
+    {
+        $this->signIn()->withExceptionHandling();
+
+        create('App\PatronCategory', ['id' => 1]);
+
+        $this->get('/logbook/store?id=6&operation=add')
+            ->assertSessionHasErrors('id');
+    }
+
+    /** @test */
+    public function it_requires_all_arguments()
+    {
+        $this->signIn()->withExceptionHandling();
+
+        create('App\PatronCategory', ['id' => 1]);
+
+        $this->get('/logbook/store?id=6')
+            ->assertSessionHasErrors('operation');
+
+        $this->get('/logbook/store?operation=add')
+            ->assertSessionHasErrors('id');
+
+        $this->get('/logbook/store')
+            ->assertSessionHasErrors('id', 'operation');
+    }
 }
