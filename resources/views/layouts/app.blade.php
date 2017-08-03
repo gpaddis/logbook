677b48770b1f122e82b10faccee7a7e956fdd5c1
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-08-03 09:26:46 +0200

    Split app view and navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,91 +13,20 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-
-                        <li>
-                            <a href="{{ route('logbook.index') }}">Logbook</a>
-                        </li>
-
-                        <li>
-                            <a href="{{ route('livecounter.index') }}">Live Counter</a>
-                        </li>
-
-                        <li>
-                            <a href="{{ route('patron-categories.index') }}">Patron Categories</a>
-                        </li>
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                             Settings<span class="caret"></span>
-                         </a>
-                         <ul class="dropdown-menu" aria-labelledby="settings">
-                            <li><a href="#">Patron Categories</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">All Settings</a></li>
-                        </ul>
-                    </li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->first_name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                Logout
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    </ul>
-                </li>
-                @endif
-            </ul>
-        </div>
+        <!-- Navbar start -->
+        @include('layouts.partials.navbar')
+        <!-- Navbar end -->
+        
+        <!-- Content start -->
+        @yield('content')
+        <!-- Content end -->
     </div>
-</nav>
 
-@yield('content')
-</div>
-
-<!-- Scripts -->
-@include('layouts.partials.scripts')
-
+    <!-- Scripts start -->
+    @include('layouts.partials.scripts')
+    <!-- Scripts end -->
 </body>
 </html>
