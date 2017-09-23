9ce358cf0efda22a45d0d43c3d483194ef831b88
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-09-23 20:18:31 +0200

    Extract LogbookUpdateForm methods, fix record replacement bug
    Now if we edit an existing number of visits in the logbook form, these will be replaced in the DB (and not added to the existing ones, as it happened to be).

--- app/Http/Requests/LogbookUpdateForm.php
+++ app/Http/Requests/LogbookUpdateForm.php
@@ -26,10 +26,10 @@ class LogbookUpdateForm extends FormRequest
     public function rules()
     {
         return [
-        'entry.*.start_time' => 'required|date|before_or_equal:' . Carbon::now()->toDateTimeString(),
-        'entry.*.end_time' => 'required|date|after:start_time',
-        'entry.*.patron_category_id' => 'required|exists:patron_categories,id',
-        'entry.*.visits' => 'nullable|integer|min:0',
+            'entry.*.start_time' => 'required|date|before_or_equal:' . Carbon::now()->toDateTimeString(),
+            'entry.*.end_time' => 'required|date|after:start_time',
+            'entry.*.patron_category_id' => 'required|exists:patron_categories,id',
+            'entry.*.visits' => 'nullable|integer|min:0',
         ];
     }
 
@@ -41,7 +41,7 @@ class LogbookUpdateForm extends FormRequest
     public function messages()
     {
         return [
-        'before_or_equal' => 'You cannot save a logbook entry in the future.'
+            'before_or_equal' => 'You cannot save a logbook entry in the future.'
         ];
     }
 
@@ -86,20 +86,47 @@ class LogbookUpdateForm extends FormRequest
         // TODO: refactor this mess
         foreach ($this->input('entry.*') as $entry) {
             if ($entry['visits'] > 0) {
-                for ($i = 0; $i < $entry['visits']; $i++) {
-                    LogbookEntry::create([
-                        'patron_category_id' => $entry['patron_category_id'],
-                        'visited_at' => $entry['start_time'],
-                        ]);
-                }
+                $this->createOrReplaceEntries($entry);
             }
 
             // Keep it == 0, cause with === 0 it does not delete the entries
             if ($entry['visits'] == 0) {
-                LogbookEntry::within($entry['start_time'], $entry['end_time'])
-                ->where('patron_category_id', $entry['patron_category_id'])
-                ->delete();
+                $this->deleteEntries($entry);
             }
         }
+    }
+
+    /**
+     * Create a number of entries equal to the value of $entry['visits'] for a
+     * given patron category, or replace the existing entries for the timeslot
+     * with the new entries.
+     *
+     * @param  array  $entry
+     * @return void
+     */
+    public function createOrReplaceEntries(array $entry)
+    {
+        $this->deleteEntries($entry);
+
+        for ($i = 0; $i < $entry['visits']; $i++) {
+            LogbookEntry::create([
+                'patron_category_id' => $entry['patron_category_id'],
+                'visited_at' => $entry['start_time'],
+            ]);
+        }
+    }
+
+    /**
+     * Delete records for a given patron categories within a time range.
+     *
+     * @param  array $entry
+     *
+     * @return void
+     */
+    protected function deleteEntries(array $entry)
+    {
+        LogbookEntry::within($entry['start_time'], $entry['end_time'])
+        ->where('patron_category_id', $entry['patron_category_id'])
+        ->delete();
     }
 }
