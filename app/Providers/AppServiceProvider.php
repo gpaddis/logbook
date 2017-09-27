8295af1f3f978368e5088ed1a6e5decaf3b54ce2
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-09-27 15:28:00 +0200

    Set default string length in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /**
+         * Fix MySQL < 5.7.7 release or MariaDB older than the 10.2.2 release issue:
+         * SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;
+         * max key length is 767 bytes.
+         *
+         * @url https://laravel.com/docs/master/migrations#creating-indexes
+         * @url https://laravel-news.com/laravel-5-4-key-too-long-error
+         */
+        Schema::defaultStringLength(191);
+
         // \View::composer([
         //         'logbook.livecounter.index'
         //     ], function($view) {
