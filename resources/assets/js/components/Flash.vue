67221c40507d5a069fe4f0b3233a06a09ab419d6
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-10-06 11:46:28 +0200

    Flash message after successful logbook form submit

--- resources/assets/js/components/Flash.vue
+++ resources/assets/js/components/Flash.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="alert alert-success alert-flash" role="alert" v-show="show">
-        <strong>Success!</strong> {{ body }}
+        <strong>Done!</strong> {{ body }}
     </div>
 </template>
 
