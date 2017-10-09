cd2469e293fbb92932d7e41953a64f87ec55e7e1
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-10-09 23:58:43 +0200

    Add lorem content to day and week views

--- resources/views/logbook/tabs/day.blade.php
+++ resources/views/logbook/tabs/day.blade.php
@@ -3,5 +3,6 @@
 @section('tab-content')
 <div class="col">
     <h3>Daily statistics</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta officiis accusantium maxime doloribus exercitationem placeat, aut ex? Reiciendis quia minima doloremque voluptatum, alias totam quasi eum. Ipsum at libero perspiciatis!</p>
 </div>
 @endsection
