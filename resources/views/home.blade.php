7531e37e5bc214872af8dcad150ab8b294fb702b
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-10-10 15:39:41 +0200

    Resize card and add dummy content to logbook home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 
 <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="col">
         <div class="card">
             <div class="card-header">Dashboard</div>
 
             <div class="card-body">
-                You are logged in!
+                <h3>Logbook Home</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate sunt eaque, illo, magnam praesentium, excepturi possimus mollitia voluptatem eum magni, fugiat ut aperiam. Consectetur beatae, impedit odit provident explicabo nam.</p>
             </div>
         </div>
     </div>
