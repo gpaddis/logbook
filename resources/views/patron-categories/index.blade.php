8913a488fa1ececcd525eb479fb2c87317a5d232
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-10-19 15:45:01 +0200

    Resize index card

--- resources/views/patron-categories/index.blade.php
+++ resources/views/patron-categories/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="row justify-content-center">
-  <div class="col-md-8">
+  <div class="col">
     <div class="card">
       <div class="card-header">List of Patron Categories</div>
 
