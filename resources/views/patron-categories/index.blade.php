63c6ece7b15bbb206c5606656256f085014dce4a
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-10-23 10:29:12 +0200

    Display add new category button when db table is empty

--- resources/views/patron-categories/index.blade.php
+++ resources/views/patron-categories/index.blade.php
@@ -6,62 +6,59 @@
   <div class="col">
     <div class="card">
       <div class="card-header">List of Patron Categories</div>
-
-      @if($patron_categories->isEmpty())
       <div class="card-body">
-        @include('layouts.partials.no-patron-categories')
-      </div>
-      @else
-      <div class="card-body">
+        @if($patron_categories->isNotEmpty())
         <div class="table-responsive">
           <table class="table table-hover">
             <thead class="thead-default">
-             <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Abbreviation</th>
-              <th>Type</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($patron_categories as $category)
-            <tr>
-              <th scope="row">
-                {{ $category->id }}
-              </th>
-              <td>
-                <a href="{{ $category->path() }}">
-                  {{ $category->name }}
-                </a>
-              </td>
-              <td>
-                {{ $category->abbreviation }}
-              </td>
-              <td>
-                {{ $category->is_primary ? 'Primary' : 'Secondary' }}
-              </td>
-              <td>
-                {{ $category->is_active ? 'Active' : 'Not active' }}
-              </td>
-              <td>
-                <a href="{{ $category->path() . '/edit' }}" class="btn btn-outline-secondary btn-sm">Edit</a>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Abbreviation</th>
+                <th>Type</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($patron_categories as $category)
+              <tr>
+                <th scope="row">
+                  {{ $category->id }}
+                </th>
+                <td>
+                  <a href="{{ $category->path() }}">
+                    {{ $category->name }}
+                  </a>
+                </td>
+                <td>
+                  {{ $category->abbreviation }}
+                </td>
+                <td>
+                  {{ $category->is_primary ? 'Primary' : 'Secondary' }}
+                </td>
+                <td>
+                  {{ $category->is_active ? 'Active' : 'Not active' }}
+                </td>
+                <td>
+                  <a href="{{ $category->path() . '/edit' }}" class="btn btn-outline-secondary btn-sm">Edit</a>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+        @else
+        <p class="text-center">There are no patron categories yet.</p>
+        @endif
+
+        <div class="text-center">
+          <a href="{{ route('patron-categories.create') }}" class="btn btn-primary">Add a new patron category</a>
+        </div>
       </div>
 
-      <div class="text-center">
-        <a href="{{ route('patron-categories.create') }}" class="btn btn-primary">Add a new patron category</a>
-      </div>
-      @endif
     </div>
-
   </div>
-</div>
 </div>
 
 @endsection
