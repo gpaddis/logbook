bb12481b22f445f49e386af680da9f96af9b91d9
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-11-19 18:04:27 +0100

    Disable still empty tabs

--- resources/views/logbook/index.blade.php
+++ resources/views/logbook/index.blade.php
@@ -13,12 +13,12 @@
           <li class="nav-item">
             <a class="nav-link" href="{{ route('logbook.index') }}">Overview</a>
           </li>
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <a class="nav-link" href="{{ route('logbook.day') }}">Day</a>
           </li>
           <li class="nav-item">
             <a class="nav-link">Month</a>
-          </li>
+          </li> --}}
           <li class="nav-item">
             <a class="nav-link" href="{{ route('logbook.year') }}">Year</a>
           </li>
