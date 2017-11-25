8f4b78731d42f081810f890befecc93e663ab328
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-11-25 17:40:57 +0100

    Create a central store to keep shared state

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,25 @@
 require('./bootstrap');
 
 /**
+ * Then we create our central store, the state container that will share
+ * what is needed when the components need to access it.
+ */
+var store = {
+    debug: true,
+    state: {
+        datasets: []
+    },
+    addDataset(dataset) {
+        if (this.debug) console.log('dataset added:', dataset);
+        this.state.datasets.push(dataset);
+    },
+    clearDatasets() {
+        if (this.debug) console.log('clearDatasets action triggered');
+        this.state.datasets = [];
+    }
+}
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
@@ -20,6 +39,7 @@ Vue.component('flash', require('./components/flash.vue'));
 Vue.component('category-cards', require('./components/livecounter/category-cards.vue'));
 
 // Charts & reports
+Vue.component('selector-years', require('./components/charts/SelectorYears.vue'));
 Vue.component('chart-month', require('./components/charts/chart-month.vue'));
 Vue.component('chart-categories', require('./components/charts/chart-categories.vue'));
 Vue.component('table-report', require('./components/table-report.vue'));
